0921464e7dfb9ffffc1fd9206007ed39e558a62c
Michel Daviot <michel@daviot.info>
2019-08-20 18:22:05 +0200

    :arrow_up: Upgrade POI (fix compilation)

--- dict/src/main/java/com/tyrcho/dictionary/util/ExcelExporter.java
+++ dict/src/main/java/com/tyrcho/dictionary/util/ExcelExporter.java
@@ -7,14 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.PrintSetup;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 
 public final class ExcelExporter {
 	private ExcelExporter() {
@@ -89,10 +82,10 @@ public final class ExcelExporter {
 		monthFont.setFontHeightInPoints((short) 24);
 		monthFont.setColor(IndexedColors.WHITE.getIndex());
 		style = wb.createCellStyle();
-		style.setAlignment(CellStyle.ALIGN_CENTER);
-		style.setVerticalAlignment(CellStyle.VERTICAL_CENTER);
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
 		style.setFillForegroundColor(IndexedColors.GREY_50_PERCENT.getIndex());
-		style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 		style.setFont(monthFont);
 		style.setWrapText(true);
 		styles.put("header", style);
